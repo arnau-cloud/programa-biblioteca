2ec13a40ffc4ad7884265826c9b23f8160524d68
Arnau <arnau.costa@insllagostera.cat>
2026-01-21 10:57:05 +0100

    new file:   Bibliteca.py 	new file:   "Biblteca_funci\303\263ns.py" 	new file:   "__pycache__/Biblteca_funci\303\263ns.cpython-313.pyc"

---
+++ Bibliteca.py
@@ -0,0 +1,8 @@
+from Biblteca_funcións import *
+
+llibres = [{"Títol: ": "", "Autor: ": "", "Any-publicació: ": int(), "Prestat: ": bool()}]
+
+afegir_llib(llibres)
+elim_llibre()
+
+print(llibres)
---
+++ Biblteca_funcións.py
@@ -0,0 +1,31 @@
+#Funció per a afegir llibres
+def afegir_llib(db):
+    dades = dict()
+    for categoria in zip(db[0].keys(), db[0].values()):
+        while True:
+            try:
+                print(type(categoria[1]))
+                match categoria[1]:
+                    case bool():
+                        print("bool")
+                        dades.update({categoria[0] : bool(input(f"Introdueix la seguent dada: {categoria[0]}"))})
+                    case int():
+                        dades.update({categoria[0] : int(input(f"Introdueix la seguent dada: {categoria[0]}"))})
+                    case str():
+                        dades.update({categoria[0] : input(f"Introdueix la seguent dada: {categoria[0]}")})
+                break
+            except ValueError:
+                pedro_sanchez = str()
+                match categoria[1]:
+                    case bool():
+                        pedro_sanchez = "un bolea(True/False)"
+                    case int():
+                        pedro_sanchez = "un nombre"
+                    case str():
+                        pedro_sanchez = "un text"
+                print(f"incompatible, la dada ha de ser {pedro_sanchez}")
+    db.append(dades)
+
+
+def elim_llibre():
+    print("eliminant llibre")
